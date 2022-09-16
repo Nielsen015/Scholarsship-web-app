764c874b744b4779e282f6b813f0af94af7f6d89
Nielsen015 <mosesnielsen68@gmail.com>
2022-09-16 03:12:05 +0300

    update

--- letter.php
+++ letter.php
@@ -288,10 +288,45 @@
             </div>
             <!-- <img src="images/log.png" alt="logo" class="footer-img" > -->
             <form class="letter" action="index.php" method="post">
+                <?php 
+    $userEmail = ""; //first we leave email field blank
+    if(isset($_POST['subscribe'])){ //if subscribe btn clicked
+      $userEmail = $_POST['email']; //getting user entered email
+      if(filter_var($userEmail, FILTER_VALIDATE_EMAIL)){ //validating user email
+        $subject = "Thanks for Subscribing us - CodingNepal";
+        $message = "Thanks for subscribing to our blog. You'll always receive updates from us. And we won't share and sell your information.";
+        $sender = "From: molbstech@gmail.com";
+        //php function to send mail
+        if(mail($userEmail, $subject, $message, $sender)){
+          ?>
+           <!-- show sucess message once email send successfully -->
+          <div class="alert success-alert">
+            <?php echo "Thanks for Subscribing us." ?>
+          </div>
+          <?php
+          $userEmail = "";
+        }else{
+          ?>
+          <!-- show error message if somehow mail can't be sent -->
+          <div class="alert error-alert">
+          <?php echo "Failed while sending your mail!" ?>
+          </div>
+          <?php
+        }
+      }else{
+        ?>
+        <!-- show error message if user entered email is not valid -->
+        <div class="alert error-alert">
+          <?php echo "$userEmail is not a valid email address!" ?>
+        </div>
+        <?php
+      }
+    }
+    ?>
                 <h3>Newsletter</h3>
                 <p>Subscribe to Our Newsletter to get Lastest News On Scholarships</p>
                 <div class="subscribe">
-                    <input type="email" name="email" id="email" placeholder="Your Email Address" required>
+                    <input type="email" name="email" id="email" placeholder="Your Email Address" required value="<?php echo $userEmail ?>">
                     <button type="submit" name="subscribe" value="subscribe" class="btn">Subscribe</button>
                 </div>
             </form>
