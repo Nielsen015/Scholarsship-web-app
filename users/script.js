16b9b81428ea47cea1933e3d50375d2c272fbda9
Nielsen015 <mosesnielsen68@gmail.com>
2022-10-03 02:01:20 +0300

    update

--- users/script.js
+++ users/script.js
@@ -20,3 +20,16 @@ themeToggle.addEventListener('click', () => {
     themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+// fill table
+List.forEach(list => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                    <td>${list.name}</td>
+                    <td>${list.award}</td>
+                    <td>${list.deadline}</td>
+                    <td class="primary">Details</td>
+                    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
